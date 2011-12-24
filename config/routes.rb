4bd7daa5fb4af373f78ab7c8161a68aff53b6648
Daniel Lete <daniel.lete@heanet.ie>
2011-12-24 14:47:17 +0000

    mv public/index.html and make workdays/index to show what user is loged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Fichas::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
-  root :to => "users#new"
+#  root :to => "users#new"
+  root :to => "sessions#new"
 
   resources :sessions
 
