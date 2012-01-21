bc805f78faf8fcbd7094e8c08516d342d1a646c0
Daniel Lete <daniel.lete@gmail.com>
2012-01-21 22:04:00 +0000

    clean syntax in User views

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   has_many :submissions, :class_name => 'Submission', :foreign_key => 'submitter_id'
   has_many :approvals, :class_name => 'Submission', :foreign_key => 'approver_id'
 
+# http://kconrails.com/2010/01/29/has_and_belongs_to_many-associations-in-ruby-on-rails/
+# difference between has_many :through and has_and_belongs_to_many
   has_many :departments_managers
   has_many :departments_managed, :through => :departments_managers, :source => 'department'
 
