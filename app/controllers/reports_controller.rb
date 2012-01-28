1e685b2b6e3c2dd97fcbb406519a27162c2bd1ed
Daniel Lete <daniel.lete@gmail.com>
2012-01-28 23:35:37 +0000

    add average/week to birdseye report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,14 @@ class ReportsController < ApplicationController
 # http://www.citizensinformation.ie/en/employment/employment_rights_and_conditions/hours_of_work/working_week.html
 
   def birdseye
+    @date = params[:date_in_gui] ? Date.parse(params[:date_in_gui]) : Date.today
     @users = User.all
     @months = Date::MONTHNAMES
 #    @months = Date::MONTHNAMES.compact
   end
 
   def my_submissions
+    @date = params[:date_in_gui] ? Date.parse(params[:date_in_gui]) : Date.today
 #    @user = current_user
     @months = Date::MONTHNAMES
   end
