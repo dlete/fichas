d5e12f40ae60e9b521c337d2fec5d9a5267d1989
Daniel Lete <daniel.lete@gmail.com>
2012-01-31 17:28:27 +0000

    add validation to departments_members

--- app/models/departments_member.rb
+++ app/models/departments_member.rb
@@ -2,6 +2,8 @@ class DepartmentsMember < ActiveRecord::Base
   belongs_to :department
   belongs_to :user
 
+  # to avoid errors in validation
+  # http://stackoverflow.com/questions/1793595/rails-validation-error-with-select-tags
   # user can only be once in a department
   validates :department_id, :uniqueness => { :scope => :user_id, :message => "staff can only belong to a department once" }
   # user can only have one administrative_department
